8b3e37280fd3a6b3cc0a7d8f32e87956daeba2df
Max Theisen <96209029+MaxThFe@users.noreply.github.com>
2022-09-01 11:43:57 +0200

    clean_repo

---
+++ main.py
@@ -0,0 +1,130 @@
+import argparse
+import os
+if os.name == 'nt': # only for windows
+    from ctypes.wintypes import LONG
+import datetime
+import time
+
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torch.nn.utils import clip_grad_norm_
+from decoder.utils import convert_cycle
+
+from model import DGMG_VAE
+
+def main(opts):
+    t1 = time.time()
+
+    # Setup dataset and data loader
+    if opts['dataset'] == 'cycles':
+        from data.cycles import CycleDataset, CycleModelEvaluation, CyclePrinting
+
+        dataset = CycleDataset(fname=opts['path_to_dataset'])
+        evaluator = CycleModelEvaluation(v_min=opts['min_size'],
+                                         v_max=opts['max_size'],
+                                         dir=opts['log_dir'])
+        printer = CyclePrinting(num_epochs=opts['nepochs'],
+                                num_batches=opts['ds_size'] // opts['batch_size'])
+    else:
+        raise ValueError('Unsupported dataset: {}'.format(opts['dataset']))
+
+    data_loader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=0,
+                             collate_fn=dataset.collate_single)
+
+    # Initialize_model
+    model = DGMG_VAE(opts)
+    
+    # Initialize optimizer
+    if opts['optimizer'] == 'Adam':
+        optimizer = Adam(model.parameters(), lr=opts['lr'])
+    else:
+        raise ValueError('Unsupported argument for the optimizer')
+
+    t2 = time.time()
+
+    # Training
+    model.train()
+    for epoch in range(opts['nepochs']):
+        batch_count = 0
+        batch_loss = 0
+        optimizer.zero_grad()
+
+        for i, data in enumerate(data_loader):
+            x, edge_index = convert_cycle(data)
+            loss = model(x, edge_index, actions=data) # train on data
+            loss.backward() # backpropagate
+
+            batch_loss += loss.item()
+            #batch_prob += prob_averaged.item()
+            batch_count += 1
+ 
+            if batch_count % opts['batch_size'] == 0:
+                printer.update(epoch + 1, {'averaged_loss': batch_loss/opts['batch_size']})
+
+                if opts['clip_grad']:
+                    clip_grad_norm_(model.parameters(), opts['clip_bound'])
+
+                optimizer.step()
+
+                batch_loss = 0
+                #
+                optimizer.zero_grad()
+
+    t3 = time.time()
+
+    model.eval()
+    evaluator.rollout_and_examine(model, opts['num_generated_samples'])
+    evaluator.write_summary()
+
+    t4 = time.time()
+
+    print('It took {} to setup.'.format(datetime.timedelta(seconds=t2-t1)))
+    print('It took {} to finish training.'.format(datetime.timedelta(seconds=t3-t2)))
+    print('It took {} to finish evaluation.'.format(datetime.timedelta(seconds=t4-t3)))
+    print('--------------------------------------------------------------------------')
+    #print('On average, an epoch takes {}.'.format(datetime.timedelta(
+    #    seconds=(t3-t2) / opts['nepochs'])))
+
+    del model.g
+    torch.save(model, './model.pth')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Test')
+
+    # configure
+    parser.add_argument('--seed', type=int, default=9284, help='random seed')
+
+    #encoder
+    parser.add_argument('--enc_in_channels', type=int, default=1433, help='in_channels')
+    parser.add_argument('--enc_hidden_channels', type=int, default=32, help='hidden_channels')
+    parser.add_argument('--enc_out_channels', type=int, default=16, help='out_channels')
+    
+
+    # dataset
+    parser.add_argument('--dataset', choices=['cycles'], default='cycles',
+                        help='dataset to use')
+    parser.add_argument('--path-to-dataset', type=str, default='./data/cycles.p',
+                        help='load the dataset if it exists, '
+                             'generate it and save to the path otherwise')
+
+    # log
+    parser.add_argument('--log-dir', default='./results',
+                        help='folder to save info like experiment configuration '
+                             'or model evaluation results')
+
+    # optimization
+    parser.add_argument('--batch-size', type=int, default=20,
+                        help='batch size to use for training')
+    parser.add_argument('--clip-grad', action='store_true', default=True,
+                        help='gradient clipping is required to prevent gradient explosion')
+    parser.add_argument('--clip-bound', type=float, default=0.25,
+                        help='constraint of gradient norm for gradient clipping')
+    parser.add_argument('--reg', type=float, default=1, help='regularization for KL loss')
+
+    args = parser.parse_args()
+    
+    from decoder.utils import setup
+    opts = setup(args)
+
+    main(opts)
---
+++ model.py
@@ -0,0 +1,57 @@
+import argparse
+import os
+if os.name == 'nt': # only for windows
+    from ctypes.wintypes import LONG
+import datetime
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch_geometric.nn.models import VGAE
+from torch_geometric.nn import Sequential
+from torch_geometric.nn.conv import GCNConv
+from torch_geometric.nn.aggr import SumAggregation
+
+
+from decoder.model import *
+
+class GCNEncoder(nn.Module):
+    # encoder model based on GCNConv
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(GCNEncoder, self).__init__()
+        self.gcn_shared = GCNConv(in_channels, hidden_channels)
+        self.gcn_mu = Sequential('x, edge_index', [(GCNConv(hidden_channels, out_channels), 'x, edge_index -> x'),SumAggregation()])
+        self.gcn_logvar = Sequential('x, edge_index', [(GCNConv(hidden_channels, out_channels), 'x, edge_index -> x'), SumAggregation()])
+
+    def forward(self, x, edge_index):
+
+        edge_weight=torch.ones(len(edge_index[0]), dtype=torch.float) 
+        x = F.relu(self.gcn_shared(x, edge_index, edge_weight=edge_weight))
+        mu = self.gcn_mu(x, edge_index)
+        logvar = self.gcn_logvar(x, edge_index)
+        return mu, logvar
+
+
+class DGMG_VAE(VGAE):
+    def __init__(self, args):
+        super(DGMG_VAE, self).__init__(encoder=GCNEncoder(-1,
+                                                          args['enc_hidden_channels'],
+                                                          args['enc_out_channels']),
+                                       decoder=DGMG(args['max_size'], int(args['enc_out_channels']/2), 2),
+                                       )
+        self.reg = args['reg']
+        self.out_dim = args['enc_out_channels']
+    
+    def forward(self, x, edge_index, actions=None):
+        z = self.encode(x, edge_index)
+        loss_rec = self.decoder.forward(actions = actions, latent_z=z) # reconstrution loss
+        loss_kl = self.kl_loss() # Kubler-Leibler loss
+        #print(loss_rec, loss_kl)
+        return - loss_rec + self.reg*loss_kl 
+
+    def test_generation(self):
+        z = torch.normal(torch.zeros(self.out_dim),torch.ones(self.out_dim)).view(1,-1)
+        generated_sample = self.decoder.forward(latent_z=z)
+        return generated_sample
